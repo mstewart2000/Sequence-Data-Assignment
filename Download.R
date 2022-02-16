e11978cd9f2595868edf7f8ab08d84968baed78e
mstewart2000 <17mdms@queensu,ca>
2022-02-16 17:08:57 -0500

    Part 1

---
+++ Download.R
@@ -0,0 +1,9 @@
+ncbi_ids <- c("HQ433692.1","HQ433694.1","HQ433691.1") # NCBI IDs of sequences
+library(rentrez)  # you may need install.packages first
+Bburg <- entrez_fetch(db = "nuccore", id = ncbi_ids, rettype = "fasta") #Get data files from NCBI database
+Sequences <- strsplit(Bburg, split = "\n\n")
+Sequences <- unlist(Sequences)
+header <- gsub("(^>.*seqeunce)\\n[ATCG].*","\\1", Sequences)
+seq <- gsub("^>.*sequence\\n([ATCG].*)","\\1",Sequences)
+Sequences <- data.frame(Name=header, Sequence=seq)
+write.csv(Sequences, file = "Sequences.csv")
